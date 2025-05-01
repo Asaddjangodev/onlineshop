8606ec29ac7c99a2a87ef80f59ef27092c387f67
Asad Khoshimov <hoshimovaustin0550@gmail.com>
2025-05-01 16:05:43 +0900

    - Set up session expiration - Implemented storing shopping carts in sessions

--- myshop/settings.py
+++ myshop/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
